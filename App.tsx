96a1106730cb08e63902b1682df70d1196e79adf
Brahian Zapata <brahian.zapata@hotmail.com>
2024-03-12 12:40:10 -0500

    Create project RN

--- App.tsx
+++ App.tsx
@@ -1,118 +1,86 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  SafeAreaView,
+  Image,
+  Button
 } from 'react-native';
-
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  withAuthenticator,
+  useAuthenticator
+} from '@aws-amplify/ui-react-native';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const userSelector = (context: any) => [context.user];
+
+  const SignOutButton = () => {
+    const { user, signOut } = useAuthenticator(userSelector);
+    return (
+        <View style={styles.container}>
+          <Text style={styles.title}>
+            Bienvenido al Reto Técnico Front
+          </Text>
+          <Image
+            style={styles.image}
+            source={require("./assest/teral_image.jpeg")}
+          />
+          <Text style={styles.labelSecond}>
+            Te has logeado con el siguiente email:
+          </Text>
+          <Text style={styles.labelPrimary}>
+            {user.signInDetails?.loginId}
+          </Text>
+          <Button
+            title="Logout"
+            onPress={ signOut }
+          />
+        </View>
+    );
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <SignOutButton />
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: { width: 400, flex: 1, padding: 20, alignSelf: 'center' },
+  buttonContainer: {
+    backgroundColor: '#0D6EFD',
+    color: "white"
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  title: {
+    alignSelf: 'center',
+    marginBottom: 30,
+    fontSize: 40,
+    color: '#0D6EFD'
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  labelPrimary: {
+    alignSelf: 'center',
+    marginBottom: 20,
+    fontSize: 20,
+    color: '#0D6EFD'
   },
-  highlight: {
-    fontWeight: '700',
+  labelSecond: {
+    alignSelf: 'center',
+    marginBottom: 5,
+    fontSize: 20,
   },
+  image: { 
+    alignSelf: 'center',
+    width: 100, 
+    height: 100, 
+    marginBottom: 20 
+  }
 });
 
-export default App;
+export default withAuthenticator(App);
+
